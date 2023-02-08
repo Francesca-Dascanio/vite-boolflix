6dfeaf862aa41942decd94a3cbe1f774409d7abb
“Francesca <dascanio.francesca@gmail.com>
2023-02-08 14:30:21 +0100

    Inizializzazione progetto

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# vite-boolflix
+# BoolFlix
+
+-- API_Key:
+29ab9f66293ac69d30701ab20d25cec2 
+
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import { store } from './store';
+
+import AppHeader from './components/AppHeader.vue';
+import Appmain from './components/AppMain.vue';
+
+export default {
+    name: 'App',
+    components: {
+        AppHeader,
+        AppMain
+    },
+    data () {
+        return 
+    }
+}
+
+</script>
+
+<template>
+    
+    <AppHeader />
+
+    <AppMain />
+
+</template>
+
+<style>
+
+</style>
