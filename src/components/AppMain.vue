f1e431924313115cadec1ff3fab3e084a4e3b1de
“Francesca <dascanio.francesca@gmail.com>
2023-02-09 21:47:44 +0100

    Milestone 4 > aggiunta funzione per mouseover

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -44,9 +44,11 @@ export default {
             }
             else {
                 return 'fa-regular fa-star';
-            }
-
-            
+            } 
+        },
+        mouseHover: function (index) {
+            console.log(this.store.movies[index]);
+            // da modificare
         }
     }
 }
@@ -60,73 +62,117 @@ export default {
             Results:
         </div>
 
-        <div class="container flex overflow">
-            <!-- CARD - DATI MOVIES -->
-            <div class="card" v-for="movie, index in store.movies">
-                <div>
-                    <!-- <img :src="store.urlFirst + movie.poster_path" :alt="movie.title"> -->
-                    <img :src="getPoster(movie)" :alt="movie.title">
+        <div class="bg-color">
+            <div class="container flex overflow">
+                <!-- NUOVA CARD - DATI MOVIES CON HOVER -->
+                <!-- Card con solo img -->
+                <div id="card-img" class="card" v-for="movie, index in store.movies" @mouseover="mouseHover(index)">
+                    <div>
+                        <img :src="getPoster(movie)" :alt="movie.title">
+                    </div>
                 </div>
-                <ul class="not-list-style">
-                    <li>
-                        Titolo: 
-                        <span>
-                            {{ movie.title }}
-                        </span>
-                    </li>
-                    <li>
-                        Titolo originale: 
-                        <span>
-                            {{ movie.original_title }}
-                        </span>
-                    </li>
-                    <li>
-                        Lingua: 
-                        <span class="fi" :class="getFlag (movie)"> 
-                        </span>
-                    </li>
-                    <li>
-                        Voto: 
-                        <span v-for="item in stars">
-                            <font-awesome-icon :icon="getStars(item, movie)" /> 
-                        </span>
-                    </li>
-                </ul>
-            </div>
 
-            <!-- CARD - DATI SERIES -->
-            <div class="card" v-for="serie, index in store.series">
-                <div class="img-container">
-                    <img :src="getPoster(serie)" :alt="serie.title">
+                <!-- Card all'hover -->
+                <div id="card-hover" class="card not-visible" v-for="info, index in store.movies">
+                    <ul class="not-list-style p">
+                        <li>
+                            Title: 
+                            <span>
+                                {{ info.title }}
+                            </span>
+                        </li>
+                        <li>
+                            Original title: 
+                            <span>
+                                {{ info.original_title }}
+                            </span>
+                        </li>
+                        <li>
+                            Language: 
+                            <span class="fi" :class="getFlag (info)"> 
+                            </span>
+                        </li>
+                        <li>
+                            Vote average: 
+                            <span v-for="item in stars">
+                                <font-awesome-icon :icon="getStars(item, info)" /> 
+                            </span>
+                        </li>
+                        <li>
+                            Overview: 
+                            <span>
+                                {{ info.overview }}
+                            </span>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="not-list-style">
-                    <li>
-                        Titolo: 
-                        <span>
-                            {{ serie.name }}
-                        </span>
-                    </li>
-                    <li>
-                        Titolo originale: 
-                        <span>
-                            {{ serie.original_name }}
-                        </span>
-                    </li>
-                    <li>
-                        Lingua: 
-                        <span class="fi" :class=" getFlag (serie)"> 
-                        </span>
-                    </li>
-                    <li>
-                        Voto: {{ Math.ceil((serie.vote_average + 1)/ 2) }}
-                    </li>
-                    <li>
-                        Voto: 
-                        <span v-for="item in stars">
-                            <font-awesome-icon :icon="getStars(item, serie)" /> 
-                        </span>
-                    </li>
-                </ul>
+
+                <!-- CARD - DATI MOVIES -->
+                <!-- <div class="card" v-for="movie, index in store.movies">
+                    <div>
+                        <img :src="getPoster(movie)" :alt="movie.title">
+                    </div>
+                    <ul class="not-list-style">
+                        <li>
+                            Titolo: 
+                            <span>
+                                {{ movie.title }}
+                            </span>
+                        </li>
+                        <li>
+                            Titolo originale: 
+                            <span>
+                                {{ movie.original_title }}
+                            </span>
+                        </li>
+                        <li>
+                            Lingua: 
+                            <span class="fi" :class="getFlag (movie)"> 
+                            </span>
+                        </li>
+                        <li>
+                            Voto: 
+                            <span v-for="item in stars">
+                                <font-awesome-icon :icon="getStars(item, movie)" /> 
+                            </span>
+                        </li>
+                    </ul>
+                </div> -->
+
+                <!-- CARD - DATI SERIES -->
+                <div class="card" v-for="serie, index in store.series">
+                    <div class="img-container">
+                        <img :src="getPoster(serie)" :alt="serie.title">
+                    </div>
+                    <ul class="not-list-style">
+                        <li>
+                            Titolo: 
+                            <span>
+                                {{ serie.name }}
+                            </span>
+                        </li>
+                        <li>
+                            Titolo originale: 
+                            <span>
+                                {{ serie.original_name }}
+                            </span>
+                        </li>
+                        <li>
+                            Lingua: 
+                            <span class="fi" :class=" getFlag (serie)"> 
+                            </span>
+                        </li>
+                        <li>
+                            Voto: {{ Math.ceil((serie.vote_average + 1)/ 2) }}
+                        </li>
+                        <li>
+                            Voto: 
+                            <span v-for="item in stars">
+                                <font-awesome-icon :icon="getStars(item, serie)" /> 
+                            </span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </main>
@@ -153,6 +199,8 @@ export default {
 
                 border: 1px solid black;
                 border-right: 0.5px;
+                width: 342px;
+                height: 513px;
 
                 .img-container {
                     max-width: 342px;
