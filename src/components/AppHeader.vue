d052387d955d2cf8695d5c8070b0c499fc0f0102
“Francesca <dascanio.francesca@gmail.com>
2023-02-09 16:14:57 +0100

    Milestone 4 > stilizzazione header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,9 +8,6 @@ export default {
             store,
             navList: [
                 {
-                    label: 'BOOLFLIX'
-                },
-                {
                     label: 'Home'
                 },
                 {
@@ -40,14 +37,12 @@ export default {
     <header>
 
         <nav class="bg-color">
-            <div class="container flex justify-content-btw">
+            <div class="container flex justify-content-btw align-items-center">
                 <div class="nav-1">
-                    <ul class="not-list-style flex" >
-                        <!-- <li>
-                            <a href="#">
-                                <img src="../assets/logo.png" alt="Logo">
-                            </a>
-                        </li> -->
+                    <ul class="not-list-style flex align-items-center" >
+                        <li>
+                            <img src="../assets/netflix_logo.png" alt="logo">
+                        </li>
                         <li v-for="label, i in navList">
                             <a href="#">
                                 {{ navList[i].label }}
@@ -62,7 +57,7 @@ export default {
                     <form action="" @submit.prevent="$emit('search')">
                         <input type="text" placeholder="Search.." name="search" v-model="store.inputSearch">
                         <button type="submit">
-                            Click
+                            Go
                         </button>
                     </form>
                 </div>
@@ -75,16 +70,47 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
 
 .nav-1 {
-    li {
-        padding: 1rem 2rem 1rem 0;
+
+    width: 70%;
+
+    li:first-child {
+
+        padding: 0;
+        width: 20%;
+
+        img {
+            width: 100%;
+        }
     }
+
+    li {
+        padding: 2rem 1rem;
+
+        a {
+            font-size: 0.8rem;
+        }
+    }
+
 }
 
 .nav-2 {
+
+    width: 30%;
+
     form {
         padding: 1rem 0 1rem 1rem;
+
+        input {
+            @include input
+        }
+
+        button {
+            @include button
+        }
     }
 }
 
