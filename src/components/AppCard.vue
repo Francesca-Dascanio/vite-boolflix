b97ab51ebdcbd58cb6913121c64b0550b45fe5a3
“Francesca <dascanio.francesca@gmail.com>
2023-02-10 15:05:20 +0100

    Aggiunto componente AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,151 @@
+<script>
+import { store } from '../store';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+export default {
+    name: 'AppCard',
+    data () {
+        return {
+            store,
+            stars: [1, 2, 3, 4, 5]
+        }
+    },
+    props: {
+        element: Object
+    },
+    methods: {
+        // Per come ho strutturato non posso convertire stars in computed perchè ho un parametro 
+        getStars: function (item) {
+
+                if (item <= Math.ceil((this.element.vote_average + 1)/ 2)) {
+
+                    return 'fa-solid fa-star';
+
+                }
+                else {
+                    return 'fa-regular fa-star';
+                } 
+        }
+    },
+    computed: {
+        flag () {
+            if (this.element.original_language == 'en') {
+                    return 'fi-gb'
+                }
+                else if (this.element.original_language == 'ja') {
+                    return 'fi-jp'
+                }
+                else {
+                    return `fi-${this.element.original_language}`;
+                }
+        },
+        poster () {
+            if (this.element.poster_path == null) {
+                    return 'https://via.placeholder.com/342x513';
+                    
+                }
+                else {
+                    return this.store.urlFirst + this.element.poster_path;
+                }
+        },
+    }
+}
+
+</script>
+
+<template>
+
+    <!-- Questa è già una singola card -->
+    <div>
+         <!-- Immagine -->
+        <div id="img-container" @click="element.hovered = true"
+        :class="{
+            'not-visible': element.hovered == true,
+            '': element.hovered == false
+            }" >
+            <img :src="poster" :alt="element.title">
+        </div>
+
+        <!-- Informazioni -->
+        <ul id="info-container" class="not-list-style p" @click="element.hovered = false"
+            :class="{
+            'not-visible': element.hovered == false,
+            '': element.hovered == true
+            }" >
+            <li>
+                <strong>
+                    Title: 
+                </strong>
+                <span>
+                    {{ element.title || element.name }}
+                </span>
+            </li>
+            <li>
+                <strong>
+                    Original title: 
+                </strong>
+                <span>
+                    {{ element.original_title || element.original_name }}
+                </span>
+            </li>
+            <li>
+                <strong>
+                    Overview: 
+                </strong>
+                <span>
+                    {{ element.overview }}
+                </span>
+            </li>
+            <li>
+                <strong>
+                    Language: 
+                </strong> 
+                <span class="fi" :class="flag"> 
+                </span>
+            </li>
+            <li>
+                <strong>
+                    Vote:
+                </strong>
+                <span class="p-span">
+                    {{ Math.ceil((element.vote_average + 1)/ 2) }}
+                </span>
+                <span v-for="item in stars">
+                    <font-awesome-icon :icon="getStars(item, element)" /> 
+                </span>
+            </li>
+        </ul>
+
+    </div>
+   
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
+
+.img-container {
+                    max-width: 342px;
+                    min-height: 655px;
+
+                    img {
+                        max-width: 100%;
+                        height: 513px;
+                        object-fit: cover;
+                    }
+                }
+
+                ul {
+                    width: 342px;
+                    height: 513px;
+
+                    li {
+                        padding: 0.5rem 0;
+
+                    }
+                }
+
+
+
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,71 +1,16 @@
 <script>
 import { store } from '../store';
-import "/node_modules/flag-icons/css/flag-icons.min.css";
+import AppCard from './AppCard.vue';
+
 
 export default {
     name: 'AppMain',
+    components: {
+        AppCard
+    },
     data () {
         return {
-            store,
-            stars: [1, 2, 3, 4, 5]
-        }
-    },
-    methods: {
-        getFlag: function (object) {
-
-            if (object.original_language == 'en') {
-                return 'fi-gb'
-            }
-            else if (object.original_language == 'ja') {
-                return 'fi-jp'
-            }
-            else {
-                return `fi-${object.original_language}`;
-            }
-
-        },
-        getPoster: function (obj) {
-
-            if (obj.poster_path == null) {
-                return 'https://via.placeholder.com/342x513';
-                
-            }
-            else {
-                return this.store.urlFirst + obj.poster_path;
-            }
-
-        },
-        getStars: function (item, obj) {
-
-            if (item <= Math.ceil((obj.vote_average + 1)/ 2)) {
-
-                return 'fa-solid fa-star';
-
-            }
-            else {
-                return 'fa-regular fa-star';
-            } 
-        },
-        getHover: function (obj) {
-            
-            console.log('DENTRO A MOUSEOVER')
-
-            obj.hovered = false;
-            console.log(obj);
-
-            return obj.hovered = true;
-            
-        
-        },
-        getOut: function (obj) {
-            
-            console.log('DENTRO A MOUSEOUT')
-            
-            obj.hovered = true;
-            console.log(obj);
-
-            return obj.hovered = false;
-        
+            store
         }
     }
 }
@@ -81,116 +26,11 @@ export default {
 
         <div class="bg-color">
             <div class="container flex overflow">
-                <!-- CARD - DATI MOVIES -->
 
-                <div class="card" v-for="movie, index in store.movies" >
-                    <div id="img-container" @click="getHover(movie)"
-                    :class="{
-                        'not-visible': movie.hovered == true,
-                        '': movie.hovered == false
-                        }" >
-                        <img :src="getPoster(movie)" :alt="movie.title">
-                    </div>
-                    <ul id="info-container" class="not-list-style p" @click="getOut(movie)">
-                        <li>
-                            <strong>
-                                Title: 
-                            </strong>
-                            <span>
-                                {{ movie.title }}
-                            </span>
-                        </li>
-                        <li>
-                            <strong>
-                                Original title: 
-                            </strong>
-                            <span>
-                                {{ movie.original_title }}
-                            </span>
-                        </li>
-                        <li>
-                            <strong>
-                                Overview: 
-                            </strong>
-                            <span>
-                                {{ movie.overview }}
-                            </span>
-                        </li>
-                        <li>
-                            <strong>
-                                Language: 
-                            </strong> 
-                            <span class="fi" :class="getFlag (movie)"> 
-                            </span>
-                        </li>
-                        <li>
-                            <strong>
-                                Vote:
-                            </strong>
-                            <span class="p-span">
-                                {{ Math.ceil((movie.vote_average + 1)/ 2) }}
-                            </span>
-                            <span v-for="item in stars">
-                                <font-awesome-icon :icon="getStars(item, movie)" /> 
-                            </span>
-                        </li>
-                    </ul>
-                </div>
+                <AppCard v-for="movie, index in store.movies" :element="movie" class="card" />
 
-                <!-- CARD - DATI SERIES -->
-                <div class="card" v-for="serie, index in store.series">
-                    <div class="img-container"  @click="getHover(serie)"
-                        :class="{
-                            'not-visible': serie.hovered == true,
-                            '': serie.hovered == false
-                        }">
-                        <img :src="getPoster(serie)" :alt="serie.title">
-                    </div>
-                    <ul class="not-list-style p" @click="getOut(serie)">
-                        <li>
-                            <strong>
-                                Title: 
-                            </strong> 
-                            <span>
-                                {{ serie.name }}
-                            </span>
-                        </li>
-                        <li>
-                            <strong>
-                                Original title: 
-                            </strong>
-                            <span>
-                                {{ serie.original_name }}
-                            </span>
-                        </li>
-                        <li>
-                            <strong>
-                                Overview: 
-                            </strong>
-                            <span>
-                                {{ serie.overview }}
-                            </span>
-                        </li>
-                        <li>
-                            <strong>
-                                Language: 
-                            </strong>  
-                            <span class="fi" :class=" getFlag (serie)"> 
-                            </span>
-                        </li>
-                        <li>
-                            <strong>
-                                Vote:
-                            </strong>
-                            <span class="p-span">
-                                {{ Math.ceil((serie.vote_average + 1)/ 2) }}
-                            </span>
-                            <span v-for="item in stars">
-                                <font-awesome-icon :icon="getStars(item, serie)" /> 
-                            </span>
-                        </li>
-                    </ul>
-                </div>
+                <AppCard v-for="serie, index in store.series" :element="serie" class="card" />
+
             </div>
         </div>
     </main>
@@ -220,26 +60,6 @@ export default {
                 width: 342px;
                 height: 513px;
 
-                .img-container {
-                    max-width: 342px;
-                    min-height: 655px;
-
-                    img {
-                        max-width: 100%;
-                        height: 513px;
-                        object-fit: cover;
-                    }
-                }
-
-                ul {
-                    width: 342px;
-                    height: 513px;
-
-                    li {
-                        padding: 0.5rem 0;
-
-                    }
-                }
             }
 
         }
